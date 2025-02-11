589cee59ba924ba35fb26cb360505a0b314efeff
Vamshinath Mannem <vamshinath.m8@gmail.com>
2025-02-11 09:06:31 +0530

    Update bulkImage.py

--- bulkImage.py
+++ bulkImage.py
@@ -1,13 +1,162 @@
 import os
 import shutil,sys
-import humanize,json
+import humanize,json,psutil
 from flask import Flask, request, jsonify, render_template, send_file
 from PIL import Image
 from datetime import datetime
-sys.path.insert(1, '/home/vamshi/gitRepos/machineSetupRepoMain/')
-from getImageProps import getScoreOnly,faceArea
-from getScore import getScore
 Image.MAX_IMAGE_PIXELS = None
+
+from transformers import pipeline
+import requests,string
+import logging,cv2,random
+import face_recognition
+import numpy as np
+from cvzone.SelfiSegmentationModule import SelfiSegmentation
+segmentor = SelfiSegmentation()
+mp3_file = '/home/vamshi/mp3.mp3'
+
+
+logging.getLogger("transformers").setLevel(logging.ERROR)
+
+
+def faceArea(fl):
+    try:
+        total_pixels = Image.open(fl).size
+        faceimage = face_recognition.load_image_file(fl)
+        dim = face_recognition.face_locations(faceimage)[0]
+        area = abs(dim[3]-dim[1])*abs(dim[0]-dim[2])
+        face_area = area/(total_pixels[0]*total_pixels[1])*100
+    except Exception as e:
+        face_area=0
+    return face_area
+
+
+def findPer2(imagepath,removeBG=True):
+    img=cv2.imread(imagepath)
+    total_pixels = Image.open(imagepath).size
+    total_pixels = total_pixels[0]*total_pixels[1]
+    flnmtmp = ''.join(random.choices(string.ascii_lowercase +string.digits, k=7))
+    if removeBG:
+    
+        tmpbgimg = segmentor.removeBG(img)
+        impath = os.path.dirname(imagepath)
+        cv2.imwrite(impath+"/"+flnmtmp+'.jpg',tmpbgimg)
+        img = cv2.imread(impath+"/"+flnmtmp+'.jpg')
+
+        os.remove(impath+"/"+flnmtmp+'.jpg')
+        
+    #converting from gbr to hsv color space
+    img_HSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    #skin color range for hsv color space 
+    HSV_mask = cv2.inRange(img_HSV, (0, 15, 0), (17,170,255)) 
+    HSV_mask = cv2.morphologyEx(HSV_mask, cv2.MORPH_OPEN, np.ones((3,3), np.uint8))
+
+    #converting from gbr to YCbCr color space
+    img_YCrCb = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
+    #skin color range for hsv color space 
+    YCrCb_mask = cv2.inRange(img_YCrCb, (0, 135, 85), (255,180,135)) 
+    YCrCb_mask = cv2.morphologyEx(YCrCb_mask, cv2.MORPH_OPEN, np.ones((3,3), np.uint8))
+
+    #merge skin detection (YCbCr and hsv)
+    global_mask=cv2.bitwise_and(YCrCb_mask,HSV_mask)
+    global_mask=cv2.medianBlur(global_mask,3)
+    global_mask = cv2.morphologyEx(global_mask, cv2.MORPH_OPEN, np.ones((4,4), np.uint8))
+
+
+    hsvCount = cv2.countNonZero(HSV_mask)
+    ycrCount = cv2.countNonZero(YCrCb_mask)
+    gloCount = cv2.countNonZero(global_mask)
+
+    blackCount = [hsvCount,ycrCount,gloCount]
+
+
+    avgNon=-1
+    if abs(blackCount[0] - blackCount[1]) < abs(blackCount[1] - blackCount[2]) and abs(blackCount[0] - blackCount[1]) < abs(blackCount[0] - blackCount[2]):
+        avgNon = (blackCount[0]+blackCount[1])/2
+        #print("0,1",imagepath)
+    elif abs(blackCount[1] - blackCount[2]) < abs(blackCount[0] - blackCount[1])  and abs(blackCount[1] - blackCount[2]) < abs(blackCount[0] - blackCount[2]):
+        avgNon = (blackCount[1]+blackCount[2])/2
+        #print("1,2",imagepath)
+        
+    elif abs(blackCount[0] - blackCount[2]) < abs(blackCount[1] - blackCount[2]) and abs(blackCount[0] - blackCount[2]) < abs(blackCount[0] - blackCount[1]):
+        avgNon = (blackCount[0]+blackCount[2])/2
+        #print("0,2",imagepath)
+
+
+    ct=0
+    avnn=0
+    for  vl in blackCount:
+        if vl < 1000:
+            continue
+        ct+=1
+        avnn+=vl
+
+
+    if ct==1:
+        avgNon = avnn
+    elif ct==2:
+        avgNon = avnn/2
+
+
+    if avgNon == -1 and removeBG:
+        return findPer2(imagepath,removeBG=False)
+        
+    skinPercentage = avgNon*100/total_pixels
+
+
+    if 100 - skinPercentage > 99.000 and removeBG:
+        print(100 - skinPercentage)
+        return findPer2(imagepath,removeBG=False)
+
+
+    return round(skinPercentage,3)
+
+
+def classify_nsfw(image_path):
+    predict = pipeline("image-classification", model="AdamCodd/vit-base-nsfw-detector")
+    try:
+        img = Image.open(image_path)
+        result = predict(img)
+        return result
+    except Exception as e:
+        print(f"Error processing {image_path}: {e}")
+        return None
+    
+
+def getNSFWScoreJS(file_path,in_memory_file=False):
+    url = "http://0.0.0.0:3333/single/multipart-form"
+    if in_memory_file:
+        return 0
+        in_memory_file.seek(0)
+         
+        files = {"content": ("temp.jpg", in_memory_file, "image/jpeg")}
+    
+    try:
+        rsp = requests.get(url,timeout=3)
+    except Exception as e:
+        os.system(f"mpg123 {mp3_file}")
+        print('Error',e)
+
+
+    files = {"content": open(file_path, "rb")}
+    response = requests.post(url, files=files)
+    jsResponsee = sorted(response.json()['prediction'],key=lambda x:x['probability'],reverse=True)[0]
+    if jsResponsee['className'] in ['Sexy','Porn']:
+        jsScore = round(jsResponsee['probability'],4)
+    else:
+        jsScore=-1
+    return jsScore
+
+def isMemAvailable():
+    return psutil.virtual_memory().available > 1173549056
+
+def getNSFWScore(img1):
+    NJsScore = getNSFWScoreJS(img1)
+    addmCod = classify_nsfw(img1)
+    nsfw_score = [item['score'] for item in addmCod if item['label'] == 'nsfw'][0]
+    return round((nsfw_score+NJsScore)/2,4)
+
+
 
 app = Flask(__name__)
 IMAGES_PER_PAGE = 50
@@ -26,6 +175,15 @@ def get_images_from_directory(root_dir,sortBy,sort_order,load_last):
     except Exception as e:
         alreadySeen=[]
     finalRec=[]
+
+    alreadyCalc=set()
+    if os.path.isfile(os.path.join(root_dir,"lastLoad.jsonl")):
+        with open(os.path.join(root_dir,"lastLoad.jsonl"), "r", encoding="utf-8") as fl:
+            for line in fl:
+                rec = json.loads(line.strip())
+                alreadyCalc.add(rec['file'])
+
+
     if load_last:
         print("loaded from json")
         with open(os.path.join(root_dir,"lastLoad.jsonl"), "r", encoding="utf-8") as fl:
@@ -40,6 +198,7 @@ def get_images_from_directory(root_dir,sortBy,sort_order,load_last):
         for subdir, _, files in os.walk(root_dir):
             for file in files:
                 file_path = os.path.abspath(os.path.join(subdir, file))  # Ensure full path
+                if file_path in alreadyCalc:continue
                 if file.lower().endswith(('jpg', 'jpeg', 'png', 'bmp', 'webp', 'gif')) and file_path not in alreadySeen:
                     image_files.append([
                         file_path,
@@ -75,18 +234,29 @@ def get_images_from_directory(root_dir,sortBy,sort_order,load_last):
                 rec['size']=flsz
                 rec['hsize']=humanize.naturalsize(flsz)
                 rec['mtime']=mtime
-                tmp = getScoreOnly(img,True,True)
-                if tmp:
-                    skinPer = tmp['skinPer']
-                    nsfw_score1 = tmp['nsfw_score1']
+                try:
+                    skinPer = findPer2(img)
+                except Exception as e:
+                    print(e)
+                try:
+                    nsfw_score1 = getNSFWScore(img)
+                except Exception as e:
+                    print(e)
+
                 rec['skinPer']=skinPer
                 rec['nsfw_score']=nsfw_score1
-                finalRec.append(rec)
+                #finalRec.append(rec)
             except Exception as e:
                 print(e)
 
             with open(os.path.join(root_dir,"lastLoad.jsonl"),'a',encoding='utf-8') as fl:
                 fl.write(json.dumps(rec) + "\n") 
+            
+            if ctr%5:
+                if not isMemAvailable():
+                    print("Memory full,exiting")
+                    os.system(f"mpg123 {mp3_file}")
+                    sys.exit(0)
 
     preSort = sorted(finalRec,key= lambda x:x[sortBy],reverse=sort_order)
     finalList=[]
