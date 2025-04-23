7596a8101453d1e4bf67a60aa2e6bc61ca375644
Vamshinath Mannem <vamshinath.m8@gmail.com>
2025-04-23 22:54:45 +0530

    final

--- bulkImage.py
+++ bulkImage.py
@@ -5,156 +5,17 @@ from flask import Flask, request, jsonify, render_template, send_file
 from PIL import Image
 from datetime import datetime
 Image.MAX_IMAGE_PIXELS = None
-
-from transformers import pipeline
+from filehash import FileHash
+from tqdm import tqdm
 import requests,string
 import logging,cv2,random
 import face_recognition,psutil
 import numpy as np
-from cvzone.SelfiSegmentationModule import SelfiSegmentation
-segmentor = SelfiSegmentation()
+from pymongo import MongoClient
+mongoClient = MongoClient('mongodb://localhost:27017/')
+db = mongoClient["filesManager"]
 mp3_file = '/home/vamshi/mp3.mp3'
-
-
-logging.getLogger("transformers").setLevel(logging.ERROR)
-quick_load=False
-
-def faceArea(fl):
-    global quick_load
-    if quick_load:
-       return -9090
-    try:
-        total_pixels = Image.open(fl).size
-        faceimage = face_recognition.load_image_file(fl)
-        dim = face_recognition.face_locations(faceimage)[0]
-        area = abs(dim[3]-dim[1])*abs(dim[0]-dim[2])
-        face_area = area/(total_pixels[0]*total_pixels[1])*100
-    except Exception as e:
-        face_area=0
-    return face_area
-
-
-def findPer2(imagepath,removeBG=True):
-
-    global quick_load
-    if quick_load:
-       return -9090
-
-    img=cv2.imread(imagepath)
-    total_pixels = Image.open(imagepath).size
-    total_pixels = total_pixels[0]*total_pixels[1]
-    flnmtmp = ''.join(random.choices(string.ascii_lowercase +string.digits, k=7))
-    if removeBG:
-    
-        tmpbgimg = segmentor.removeBG(img)
-        impath = os.path.dirname(imagepath)
-        cv2.imwrite(impath+"/"+flnmtmp+'.jpg',tmpbgimg)
-        img = cv2.imread(impath+"/"+flnmtmp+'.jpg')
-
-        os.remove(impath+"/"+flnmtmp+'.jpg')
-        
-    #converting from gbr to hsv color space
-    img_HSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    #skin color range for hsv color space 
-    HSV_mask = cv2.inRange(img_HSV, (0, 15, 0), (17,170,255)) 
-    HSV_mask = cv2.morphologyEx(HSV_mask, cv2.MORPH_OPEN, np.ones((3,3), np.uint8))
-
-    #converting from gbr to YCbCr color space
-    img_YCrCb = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
-    #skin color range for hsv color space 
-    YCrCb_mask = cv2.inRange(img_YCrCb, (0, 135, 85), (255,180,135)) 
-    YCrCb_mask = cv2.morphologyEx(YCrCb_mask, cv2.MORPH_OPEN, np.ones((3,3), np.uint8))
-
-    #merge skin detection (YCbCr and hsv)
-    global_mask=cv2.bitwise_and(YCrCb_mask,HSV_mask)
-    global_mask=cv2.medianBlur(global_mask,3)
-    global_mask = cv2.morphologyEx(global_mask, cv2.MORPH_OPEN, np.ones((4,4), np.uint8))
-
-
-    hsvCount = cv2.countNonZero(HSV_mask)
-    ycrCount = cv2.countNonZero(YCrCb_mask)
-    gloCount = cv2.countNonZero(global_mask)
-
-    blackCount = [hsvCount,ycrCount,gloCount]
-
-
-    avgNon=-1
-    if abs(blackCount[0] - blackCount[1]) < abs(blackCount[1] - blackCount[2]) and abs(blackCount[0] - blackCount[1]) < abs(blackCount[0] - blackCount[2]):
-        avgNon = (blackCount[0]+blackCount[1])/2
-        #print("0,1",imagepath)
-    elif abs(blackCount[1] - blackCount[2]) < abs(blackCount[0] - blackCount[1])  and abs(blackCount[1] - blackCount[2]) < abs(blackCount[0] - blackCount[2]):
-        avgNon = (blackCount[1]+blackCount[2])/2
-        #print("1,2",imagepath)
-        
-    elif abs(blackCount[0] - blackCount[2]) < abs(blackCount[1] - blackCount[2]) and abs(blackCount[0] - blackCount[2]) < abs(blackCount[0] - blackCount[1]):
-        avgNon = (blackCount[0]+blackCount[2])/2
-        #print("0,2",imagepath)
-
-
-    ct=0
-    avnn=0
-    for  vl in blackCount:
-        if vl < 1000:
-            continue
-        ct+=1
-        avnn+=vl
-
-
-    if ct==1:
-        avgNon = avnn
-    elif ct==2:
-        avgNon = avnn/2
-
-
-    if avgNon == -1 and removeBG:
-        return findPer2(imagepath,removeBG=False)
-        
-    skinPercentage = avgNon*100/total_pixels
-
-
-    if 100 - skinPercentage > 99.000 and removeBG:
-        print(100 - skinPercentage)
-        return findPer2(imagepath,removeBG=False)
-
-
-    return round(skinPercentage,3)
-
-
-def classify_nsfw(image_path):
-    predict = pipeline("image-classification", model="AdamCodd/vit-base-nsfw-detector")
-    try:
-        img = Image.open(image_path)
-        result = predict(img)
-        return result
-    except Exception as e:
-        print(f"Error processing {image_path}: {e}")
-        return None
-    
-
-def getNSFWScoreJS(file_path,in_memory_file=False):
-    url = "http://0.0.0.0:3333/single/multipart-form"
-    
-    files = {"content": open(file_path, "rb")}
-    response = requests.post(url, files=files)
-    jsResponsee = sorted(response.json()['prediction'],key=lambda x:x['probability'],reverse=True)[0]
-    if jsResponsee['className'] in ['Sexy','Porn']:
-        jsScore = round(jsResponsee['probability'],4)
-    else:
-        jsScore=-1
-    return jsScore
-
-def isMemAvailable():
-    return psutil.virtual_memory().available > 1173549056
-
-def getNSFWScore(img1):
-    global quick_load
-    NJsScore = getNSFWScoreJS(img1)
-    if quick_load:
-        return NJsScore
-    addmCod = classify_nsfw(img1)
-    nsfw_score = [item['score'] for item in addmCod if item['label'] == 'nsfw'][0]
-
-    return round((nsfw_score+NJsScore)/2,4)
+hasher = FileHash('sha256')
 
 
 
@@ -163,10 +24,7 @@ IMAGES_PER_PAGE = 50
 root_dir=''
 def get_images_from_directory(root_dir,sortBy,sort_order,load_last):
 
-
     print(sortBy,sort_order,load_last,quick_load)
-
-
     alreadySeen = []
     try:
         with open(root_dir+"/seen.txt") as fl:
@@ -179,97 +37,121 @@ def get_images_from_directory(root_dir,sortBy,sort_order,load_last):
     alreadyCalc=set()
     if os.path.isfile(os.path.join(root_dir,"lastLoad.jsonl")):
         with open(os.path.join(root_dir,"lastLoad.jsonl"), "r", encoding="utf-8") as fl:
-            for ctr,line in enumerate(fl):
-                print(ctr)
+            for line in fl:
                 rec = json.loads(line.strip())
-                try:
-                    alreadyCalc.add(rec['file'])
-                except Exception as e:
-                    e=0
-
-
+                if rec:
+                    try:
+                        alreadyCalc.add(rec['file'])
+                    except Exception as e:
+                        e=0
     if load_last:
         print("loaded from json")
         with open(os.path.join(root_dir,"lastLoad.jsonl"), "r", encoding="utf-8") as fl:
-            for ctr,line in enumerate(fl):
-                print(ctr)
+            for line in fl:
                 rec = json.loads(line.strip())  # Convert JSON string to dictionary
-                if rec['file'] not in alreadySeen and os.path.isfile(rec['file']):
+                if rec and rec['file'] not in alreadySeen and os.path.isfile(rec['file']):
                     finalRec.append(rec)
 
 
     else:
         image_files = []
-        for subdir, _, files in os.walk(root_dir):
-            for file in files:
-                file_path = os.path.abspath(os.path.join(subdir, file))  # Ensure full path
-                if file_path in alreadyCalc:continue
-                if file.lower().endswith(('jpg', 'jpeg', 'png', 'bmp', 'webp', 'gif')) and file_path not in alreadySeen:
-                    image_files.append([
-                        file_path,
-                        os.path.getsize(file_path),
-                        os.stat(file_path).st_mtime
-                    ])
+        
+        query = {
+            "filetype": "image",
+            "isReady": True,
+            "filefullpath": {"$regex": root_dir}
+        }
+
+        cursor = list(db["files"].find(query, {"_id": 1, "filehash": 1, "filefullpath": 1}).sort("filesize", 1))
+        while cursor:
+            image_files.append(cursor.pop(0)['filefullpath'])  # smallest
+            if cursor:
+                image_files.append(cursor.pop(-1)['filefullpath'])  # largest
 
         finalRec=[]
         ctr=0
         ttl = len(image_files)
-        image_files = sorted(image_files,key=lambda x:x[1],reverse=True)
 
-        for img,flsz,mtime in image_files:
-            print(ctr,ttl)
-            ctr+=1
-            rec={}
-            try:
-                Img = Image.open(img)
-                nsfw_score1=-1
-                skinPer=-10
-                score=-1
-                w=Img.size[0]
-                h=Img.size[1]
+        print('total files',ttl)
 
+        for img in tqdm(image_files,total=ttl,unit='img'):
+            filehash = hasher.hash_file(img)
+            props = db['rootLookup'].find_one({'_id': filehash})
+
+            if props and props['props']:
+                rec={}
                 try:
-                    face_area = faceArea(img)
-                except Exception as e:
-                    face_area=-1
-                rec['file']=img
-                rec['w']=w
-                rec['h']=h
-                rec['pixels']=w*h
-                rec['face_area']=round(face_area,2)
-                rec['size']=flsz
-                rec['hsize']=humanize.naturalsize(flsz)
-                rec['mtime']=mtime
-                try:
-                    skinPer = findPer2(img)
-                except Exception as e:
-                    print(e)
-                try:
-                    nsfw_score1 = getNSFWScore(img)
-                except Exception as e:
-                    print(e)
+                    w=props['props']['width']
+                    h=props['props']['height']
 
-                rec['skinPer']=skinPer
-                rec['nsfw_score']=nsfw_score1
-                #finalRec.append(rec)
-            except Exception as e:
-                print(e)
+                    try:
+                        face_area = props['props']['faceArea']
+                    except Exception as e:
+                        face_area=0
 
-            with open(os.path.join(root_dir,"lastLoad.jsonl"),'a',encoding='utf-8') as fl:
-                fl.write(json.dumps(rec) + "\n") 
-            
-            if ctr%5:
-                if not isMemAvailable():
-                    print("Memory full,exiting")
-                    os.system(f"mpg123 {mp3_file}")
-                    #sys.exit(0)
-                    pid = os.getpid()
-                    psutil.Process(pid).terminate()
+                    rec['file']=img
+                    rec['w']=w
+                    rec['h']=h
+                    rec['pixels']=w*h
+                    rec['face_area']=round(face_area,2)
+                    try:
+                        rec['skinPer']=props['props']['skinPer']
+                    except Exception as e:
+                        rec['skinPer']=0
+
+                    rec['size']=props['filesize']
+                    rec['hsize']=humanize.naturalsize(rec['size'])
+                    rec['mtime']=props['filemtime']
+                    try:
+                        rec['nsfw_score']=props['props']['specialProps']['nsfw_score']
+                    except Exception as e:
+                        rec['nsfw_score']=-1
+
+                    try:
+                        rec['scoreAvg'] = round(props['props']['specialProps']['scoreAvg'],2)
+                    except Exception as e:
+                        rec['scoreAvg'] = -1
+
+                    exposedLabels = ['ARMPITS_EXPOSED_score','EXPOSED_ARMPITS_score',
+                         'BELLY_EXPOSED_score','EXPOSED_BELLY_score',
+                         'EXPOSED_BUTTOCKS_score','BUTTOCKS_EXPOSED_score'
+                         'EXPOSED_BREAST_F_score','FEMALE_BREAST_EXPOSED',
+                         'FEMALE_GENITALIA_EXPOSED','EXPOSED_GENITALIA_F'
+                         ]
+                    
+                    rec['exposedScore']= 0
+                    topExposedLabel='NaN'
+                    topExposedScore = -1000000
+                    vals=[]
+                    for label in exposedLabels:
+                        try:
+                            lbScore = props['props']['specialProps'][label]
+                            vals.append(lbScore)
+                            if topExposedScore < lbScore:
+                                topExposedLabel=label
+                                topExposedScore=lbScore
+                        except Exception as e:
+                            e=0
+                    try:
+                        rec['exposedScore']=max(vals)
+                    except Exception as e:
+                        rec['exposedScore']=0
+                    rec['topExposedLabel']=topExposedLabel.split('_score')[0]+'_'+str(round(topExposedScore,2))
+
+
+
+
+                except Exception as e:
+                    print(e,filehash)
+
+                with open(os.path.join(root_dir,"lastLoad.jsonl"),'a',encoding='utf-8') as fl:
+                    fl.write(json.dumps(rec) + "\n") 
+
 
     preSort = sorted(finalRec,key= lambda x:x[sortBy],reverse=sort_order)
     finalList=[]
     for rec in preSort:
-        finalList.append([rec['file'],str(rec['w'])+'x'+str(rec['h']),rec['face_area'],rec['hsize'],rec['skinPer'],rec['nsfw_score']])
+        finalList.append([rec['file'],str(rec['w'])+'x'+str(rec['h']),rec['face_area'],rec['hsize'],rec['skinPer'],rec['nsfw_score'],rec['topExposedLabel'],rec['scoreAvg']])
     return finalList
 
 @app.route('/')
